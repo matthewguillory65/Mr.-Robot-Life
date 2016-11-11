fd92a29429eb162e67da3fb0d36d939f5235dd5a
matthewguillory65 <matthew.guillor@ad.aie.edu>
2016-11-11 17:31:14 -0600

    Python work
    python work

---
+++ daat.py
@@ -0,0 +1,12 @@
+import os
+import webbrowser
+import turtle 
+
+def start():
+	webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open("https://google.com")
+	webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open("https://trello.com/b/iGX1hJxu/student-work-log")
+	webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open("https://github.com/matthewguillory65")
+	webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open("https://aielafayette.slack.com/messages/y1prog/")
+	os.system('python')
+start()
+raw_input()
---
+++ dat.py
@@ -0,0 +1,6 @@
+def Work():	
+	return "hello world"
+Aye = Work() * 100
+print Aye
+
+raw_input()
